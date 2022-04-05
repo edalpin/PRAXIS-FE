762cd8034cd985f012ac81f8807490a2ff5e3fff
Jenny Zapata Pulgarín <jmzapatap@unal.edu.co>
2022-04-04 22:01:41 -0500

    Add delete option, add interceptor, and missing data attributes

---
+++ src/app/no-found/no-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'no-found',
+  templateUrl: 'no-found.component.html',
+  styleUrls: ['no-found.component.css'],
+})
+export class NoFoundComponent {}
